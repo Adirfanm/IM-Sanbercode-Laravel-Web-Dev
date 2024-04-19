1ebb2d780f734fe0f385204a000794f56bc5e242
adiirfanm <91816687+Adirfanm@users.noreply.github.com>
2024-04-19 09:37:05 +0700

    Tugas 14 – Mengatur Database dengan Migration

---
+++ blog/database/migrations/2024_04_19_020324_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('synopsis');
+            $table->year('year');
+            $table->string('poster', 255)->nullable();
+            $table->foreignId('genre_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
